2a79bc06acc45086b688e1bb9ee55050cb1d241d
smartqrc <36959624+smartqrc@users.noreply.github.com>
2024-03-28 16:21:36 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -95,7 +95,7 @@ const CONFIG = {
   STARTER_CONTACT_EMAIL_TEXT: 'mail@tangly1024.com',
 
   // 嵌入外部表单
-  STARTER_CONTACT_MSG_EXTERNAL_URL: 'http://emvrxywtgnf05rjr.mikecrm.com/kl9PGY5', // 基于NoteForm创建，将留言数据存在Notion中
+  STARTER_CONTACT_MSG_EXTERNAL_URL: '', // 基于NoteForm创建，将留言数据存在Notion中
   //   自定义留言表单，以下几个配置暂时废弃
   //   STARTER_CONTACT_MSG_TITLE: '向我们留言',
   //   STARTER_CONTACT_MSG_NAME: '姓名',
